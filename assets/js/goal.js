f1fc7b35ab71022eb1c492ece3290ff0fc0331a9
Paul Von Zimmerman <pcvonz@gmail.com>
2017-09-05 01:42:36 +0000

    Make component styling more flexible.

--- assets/js/goal.js
+++ assets/js/goal.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var pBar = document.querySelector(".goalContainer  .percentageBar");
-  var pBarP = document.querySelector(".goalContainer > p");
+  var pBarP = document.querySelector(".goalContainer > .percentageBar > div");
 
   var goal = document.getElementById('goal').dataset.percentage;
   function animate (time) {
@@ -13,7 +13,7 @@ window.onload = function () {
       .to({x: goal}, 500)
       .onUpdate( function () {
         pBarP.innerHTML = Math.round(start.x) + "%";
-        pBar.style.width = start.x + "%";
+        pBar.style.backgroundImage = 'linear-gradient(90deg, white '+start.x+'%, #052D49 '+start.x+'%)';
       })
     .start();
 };
